28e6b17a35faddb23e15f37d0d0054097bfbdda3
PaltaConQueso <47439085+lukitasxue@users.noreply.github.com.>
2025-02-15 17:57:42 +1100

    simple array operations concepts

--- Arrays/arrayPractice.py
+++ Arrays/arrayPractice.py
@@ -1,7 +1,49 @@
 arr = [10,20,30,40,50]
 
+# add value at index, number
 arr.insert(1,15)
-arr.remove(3)
+
+# delete values from array
+# arr.pop(3)
+# arr.remove(10)
+# del arr[0]
+arr.remove(15)
+
 print(f"Numbers: ", arr)
 
+# search for number with if statement
+if 30 in arr:
+    print("30 in array")
+else:
+    print("not in array")
+
+index = arr.index(30) #finds index for 30
+print(index)
+
+element = 50
+found = False
+
+for i in range(len(arr)):
+    if arr[i] == element:
+        print("found!")
+        found = True
+        break
+
+if not found:
+    print(f"not found {element}")
+
+
+#slice parts of an array:
+#arr[start : end : step]
+
+
+
+
+
 # Hello world
+# git init
+# git remote add origin link
+# git add.
+# git commit -m "dscrpt"
+# git push origin master
+# git status
